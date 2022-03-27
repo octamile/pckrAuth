1a0822a9bd284912bf17f28b6032e1bc19136c25
Brian Ibrahim Qamardeen <qeetell@protonmail.com>
2022-03-27 21:42:01 +0100

    Progress

--- rsblVldtAthrzt.go
+++ rsblVldtAthrzt.go
@@ -15,7 +15,7 @@ func RsblVldtAthrzt (userId string, athrztDtls []string) (err error, vldty bool)
 	_x5100 := gjson.Get (athrztDtls [2], athrztIdPath).String ()
 	_x5200 := gjson.Get (athrztDtls [2], athrztKeyPath).String ()
 
-	if _x5100 == athrztDtls [0] && _x5200 == athrztDtls [1] {
+	if _x5100 != "" && _x5100 == athrztDtls [0] && _x5200 != && _x5200 == athrztDtls [1] {
 		return nil, true
 	} else {
 		return nil, false
